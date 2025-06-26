fe19d68f3012d7487ba8e801daceac7f33e9c3ef
nandini-mp <nandinimprakash.06@gmail.com>
2025-06-26 16:01:18 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <div className="navbar">
   <div className="navbar-container">
-    <a href=".././public/NandiniMPrakash_Resume.pdf" target='_blank' rel="noopener noreferrer" className="resume">Resume</a>
+    <a href="/NandiniMPrakash_Resume.pdf" target='_blank' rel="noopener noreferrer" className="resume">Resume</a>
     <ul className="nav-links">
       <li><a href="#skills">Skills</a></li>
       <li><a href="#profiles">Profiles</a></li>
